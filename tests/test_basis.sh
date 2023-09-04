ac936835bb03edee00d51fc972f6067434ac3a17
kitsuyui <kitsuyui@kitsuyui.com>
2023-09-04 13:00:50 +0900

    Improve test cleanup

--- tests/test_basis.sh
+++ tests/test_basis.sh
@@ -6,25 +6,35 @@ set -euo pipefail
 self_path=$(realpath $(dirname "$0"))
 gitignore_in_path=$(realpath "$self_path/../gitignore.in")
 
-# Create a temporary directory for testing
-tmp_dir=$(mktemp -d)
-echo "Testing in temporary directory: $tmp_dir"
-
-# Test in sub shell to clean up in case of failure
-(
+test_in_sandbox() {
+    # Create a temporary directory for testing
+    tmp_dir=$(mktemp -d)
+    echo "Testing in temporary directory: $tmp_dir"
     cd "$tmp_dir"
-    # Create a git repository
-    git init
-    # First run
-    "$gitignore_in_path"
-    # .gitignore.in should be created
-    test -f .gitignore.in && echo "OK"
+    set +e
+    # Test in forked shell
+    cat /dev/stdin | bash
+    set -e
+    if [ $? -ne 0 ]; then
+        echo "Test failed" >&2
+        exit 1
+    else
+        echo "Test passed" >&2
+    fi
+    cd - >/dev/null
+    # Clean up
+    rm -rf "$tmp_dir"
+}
 
-    # Second run
-    "$gitignore_in_path"
-    # .gitignore should be created
-    test -f .gitignore && echo "OK"
-)
-
-# Clean up
-rm -rf "$tmp_dir"
+test_in_sandbox <<EOF
+# Create a git repository
+git init
+# First run
+"$gitignore_in_path"
+# .gitignore.in should be created
+test -f .gitignore.in && echo "OK" >&2
+# Second run
+"$gitignore_in_path"
+# .gitignore should be created
+test -f .gitignore && echo "OK" >&2
+EOF
